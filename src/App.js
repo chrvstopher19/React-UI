bd04b339c0e195a96ebb5cdf3733bdb8967ffbad
chrvstopher19 <140680259+chrvstopher19@users.noreply.github.com>
2023-11-08 18:12:27 -0500

    90% done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [rickAndMort, setRickAndMort] = useState ([]);
 
+  const fetchrickandMort = async () => {
+    const res = await axios.get(
+      `https://rickandmortyapi.com/api/character`
+    );
+
+
+    const characterURLs = res.data.results.map(character => character.image);
+    console.log(characterURLs);
+
+
+    console.log(res.data)
+    setRickAndMort(res.data);
+  };
+
+
+  useEffect(() => {
+    fetchrickandMort();
+  }, []);
+console.log(rickAndMort.results[0].image)
+const fallbackImage = "https://wheelofnames.com/images/open-graph-image.png"
+  return (
+  <div className="App">
+    <div className="RnMContainer"> 
+      <h1 className='title'>Rick and Morty</h1>
+        <div className= 'img'>
+        <img src={rickAndMort.results[0]?.image || fallbackImage} alt="" />
+        </div>
+            <div className = 'RnMBtn'>
+            <button onClick={fetchrickandMort}> ← </button>
+            <button onClick={fetchrickandMort}> → </button>
+            </div>
+        </div>
+    </div>)
+  
+}
 export default App;
+
