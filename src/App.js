8e2d440fc351a66914a2c433faa572b0710e6f3f
chrvstopher19 <140680259+chrvstopher19@users.noreply.github.com>
2023-11-09 15:00:03 -0500

    React

--- src/App.js
+++ src/App.js
@@ -5,37 +5,47 @@ import './index.css';
 
 function App() {
   const [rickAndMort, setRickAndMort] = useState ([]);
+  const [index, setIndex] = useState(0)
 
   const fetchrickandMort = async () => {
     const res = await axios.get(
-      `https://rickandmortyapi.com/api/character`
-    );
+      'https://rickandmortyapi.com/api/character');
+      setRickAndMort(res.data.results);
+    };
+    
+    useEffect(() => {
+      fetchrickandMort();
+    }, []);
 
+    function next(){
+      if (index === rickAndMort.length - 1){
+        setIndex(0)
+      } else {
+        setIndex((prevIndex) => prevIndex + 1)
+      }
+    }
 
-    const characterURLs = res.data.results.map(character => character.image);
-    console.log(characterURLs);
+    function previous(){
+      if (index === 0) {
+        setIndex(rickAndMort.length - 1)
+      } else {
+        setIndex((prevIndex) => prevIndex - 1)
+      }
+    }
 
-
-    console.log(res.data)
-    setRickAndMort(res.data);
-  };
-
-
-  useEffect(() => {
-    fetchrickandMort();
-  }, []);
-console.log(rickAndMort.results[0].image)
-const fallbackImage = "https://wheelofnames.com/images/open-graph-image.png"
+  const fallbackImage = "https://wheelofnames.com/images/open-graph-image.png"
+  
   return (
   <div className="App">
     <div className="RnMContainer"> 
       <h1 className='title'>Rick and Morty</h1>
         <div className= 'img'>
-        <img src={rickAndMort.results[0]?.image || fallbackImage} alt="" />
+          <h2>{rickAndMort[index]?.name}</h2>
+          <img src={ rickAndMort[index]?.image || fallbackImage } />
         </div>
             <div className = 'RnMBtn'>
-            <button onClick={fetchrickandMort}> ← </button>
-            <button onClick={fetchrickandMort}> → </button>
+            <button onClick={previous}> ← </button>
+            <button onClick={next}> → </button>
             </div>
         </div>
     </div>)
